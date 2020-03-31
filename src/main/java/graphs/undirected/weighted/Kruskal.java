f1927d50a2e0fa8c0770e7bcd264752e378775f5
Raj Kollimarla <raj.kollimarla1@t-mobile.com>
2020-03-31 12:35:54 -0700

    Kruskal's algorithm for MST tested and works

--- src/main/java/graphs/undirected/weighted/Kruskal.java
+++ src/main/java/graphs/undirected/weighted/Kruskal.java
@@ -3,14 +3,32 @@ package graphs.undirected.weighted;
 import edu.princeton.cs.algs4.UF;
 import java.util.PriorityQueue;
 import graphs.undirected.weighted.WGraph;
+import graphs.undirected.weighted.Edge;
+import java.util.LinkedList;
 
 class Kruskal extends MST {
+	
+	private PriorityQueue<Edge> bridges;
+	private UF mst;
+
 	public Kruskal(WGraph wg) {
 		this.wg = wg;
-		
+		mst = new UF(wg.V());
+		edges = new LinkedList<Edge>();
+		weight = 0.0;
+		bridges = new PriorityQueue<Edge>();
+		for(Edge e : wg.edges())
+			bridges.add(e);
 
+		Edge e;
+		int v, w;
+		while(edges.size() < wg.V() - 1) {
+			e = bridges.poll();
+			v = e.either(); w = e.other(v);
+			if(mst.find(v) == mst.find(w)) continue;
+			edges.add(e);
+			weight += e.weight();
+			mst.union(v, w);
+		}
 	}
-
-
-
 }
---
+++ src/test/java/graphs/undirected/weighted/KruskalTest.java
@@ -0,0 +1,5 @@
+package graphs.undirected.weighted;
+
+import graphs.undirected.weighted.PrimLazyTest;
+
+class KruskalTest extends PrimLazyTest {}
